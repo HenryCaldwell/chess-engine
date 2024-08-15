79d3639301bbb1ba7ffbfa2cb7bb40b57431c21b
Henry Caldwell <henrycaldwell2005@gmail.com>
2024-08-15 02:25:45 -0400

    Added Pawn class

---
+++ src/main/java/henrycaldwell/pieces/Pawn.java
@@ -0,0 +1,73 @@
+package henrycaldwell.pieces;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+
+import henrycaldwell.Alliance;
+import henrycaldwell.board.Board;
+import henrycaldwell.board.BoardUtils;
+import henrycaldwell.board.Move;
+import henrycaldwell.board.Move.MajorMove;
+
+public class Pawn extends Piece {
+
+    private final static int[] CANDIDATE_MOVE_COORDINATES = {7, 8, 9, 16};
+
+    public Pawn(int piecePosition, Alliance pieceAlliance) {
+        super(piecePosition, pieceAlliance);
+    }
+
+    @Override
+    public Collection<Move> calculateLegalMoves(Board board) {
+        final List<Move> legalMoves = new ArrayList<>();
+
+        for (int currentCandidateOffset : CANDIDATE_MOVE_COORDINATES) {
+            int candidateDestinationCoordinate = this.piecePosition + (this.getPieceAlliance().getDirection() * currentCandidateOffset);
+
+            if (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
+                continue;
+            }
+
+            if (currentCandidateOffset == 8 && !board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
+                legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate)); // TODO needs to be edited, dealing with promotions
+            } else if (currentCandidateOffset == 16 && this.isFirstMove() && 
+                        (BoardUtils.SECOND_ROW[this.piecePosition] && this.pieceAlliance.isBlack()) || 
+                        (BoardUtils.SEVENTH_ROW[this.piecePosition] && this.pieceAlliance.isWhite())) {
+                int behindCandidateDestinationCoordinate = this.piecePosition + (this.pieceAlliance.getDirection() * 8);
+
+                if (!board.getTile(behindCandidateDestinationCoordinate).isTileOccupied() && 
+                        !board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
+                    legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+                }
+            } else if (currentCandidateOffset == 7 &&
+                        !((BoardUtils.EIGHTH_COLUMN[piecePosition] && this.pieceAlliance.isWhite() ||
+                        (BoardUtils.FIRST_COLUMN[piecePosition] && this.pieceAlliance.isBlack())))) {
+                if (board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
+                    Piece pieceOnCandidate = board.getTile(candidateDestinationCoordinate).getPiece();
+
+                    if (this.pieceAlliance != pieceOnCandidate.getPieceAlliance()) {
+                        // TODO more needs to be done here with attacking in to a pawn promotion
+                        legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+                    }
+                }
+            } else if (currentCandidateOffset == 9 &&
+                        !((BoardUtils.FIRST_COLUMN[piecePosition] && this.pieceAlliance.isWhite() ||
+                        (BoardUtils.EIGHTH_COLUMN[piecePosition] && this.pieceAlliance.isBlack())))) {
+                if (board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
+                    Piece pieceOnCandidate = board.getTile(candidateDestinationCoordinate).getPiece();
+
+                    if (this.pieceAlliance != pieceOnCandidate.getPieceAlliance()) {
+                        // TODO more needs to be done here with attacking in to a pawn promotion
+                        legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+                    }
+                }
+            }
+        }
+
+        return ImmutableList.copyOf(legalMoves);
+    }
+
+}
--- src/main/java/henrycaldwell/pieces/Piece.java
+++ src/main/java/henrycaldwell/pieces/Piece.java
@@ -10,15 +10,21 @@ public abstract class Piece {
 
     protected final int piecePosition;
     protected final Alliance pieceAlliance;
+    protected final boolean isFirstMove;
 
     public Piece (int piecePosition, Alliance pieceAlliance) {
         this.piecePosition = piecePosition;
         this.pieceAlliance = pieceAlliance;
+        this.isFirstMove = false;
     }
 
     public abstract Collection<Move> calculateLegalMoves(Board board);
 
     public Alliance getPieceAlliance() {
         return this.pieceAlliance;
+    }
+
+    public boolean isFirstMove() {
+        return this.isFirstMove;
     }
 }
