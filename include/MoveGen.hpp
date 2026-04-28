7f2900f1997daacb0346e9a1f006400432031f3d
HenryCaldwell <henrycaldwell2005@gmail.com>
2026-04-28 02:06:20 -0400

    fix(engine): add include guards to move generation header

--- include/MoveGen.hpp
+++ include/MoveGen.hpp
@@ -1,3 +1,6 @@
+#ifndef MOVEGEN_HPP
+#define MOVEGEN_HPP
+
 #include "Board.hpp"
 #include "Move.hpp"
 #include <vector>
@@ -6,3 +9,5 @@ namespace MoveGen {
   std::vector<Move> generateMoves(Board& board);
   std::vector<Move> generateCaptures(Board& board);
 }
+
+#endif
